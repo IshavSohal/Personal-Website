01f9096633a8d8aed5421bb195eda44ac2762914
Ishav Sohal <ishav.sohal@mail.utoronto.ca>
2023-01-15 02:06:23 -0500

    Added program files

---
+++ app.js
@@ -0,0 +1,15 @@
+function homeButton(){
+    document.getElementById("body").scrollIntoView();
+}
+
+function aboutButton(){
+    document.getElementById("aboutme").scrollIntoView();
+}
+
+function contactButton(){
+    document.getElementById("contactinfo").scrollIntoView();
+}
+
+function codingButton(){
+    document.getElementById("codingprojects").scrollIntoView();
+}
